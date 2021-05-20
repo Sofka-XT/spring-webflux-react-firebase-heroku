9d9623edad5eb88c15aa42d0c3aa6d05d8ca3ca7
Raul A. Alzate <alzategomez.raul@gmail.com>
2021-05-20 14:07:51 -0500

    Start project init

---
+++ src/test/java/co/com/sofka/questions/QuestionsApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.sofka.questions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
